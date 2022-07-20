a48d97574a3ea92bb0f67d06d65581d141ebc2f5
jaruiz <jaruiz@paradigmadigital.com>
2022-07-20 12:55:17 +0200

    First version

--- fraud-checker/src/main/java/com/jaruiz/examples/kstreams/fraudchecker/model/PotentialFraudCase.java
+++ fraud-checker/src/main/java/com/jaruiz/examples/kstreams/fraudchecker/model/PotentialFraudCase.java
@@ -1,31 +1,39 @@
 package com.jaruiz.examples.kstreams.fraudchecker.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.jaruiz.examples.kstreams.model.CardMovement;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = PotentialOnlineFraudCase.class),
+        @JsonSubTypes.Type(value = PotentialPhysicalFraudCase.class)
+})
 @Data
-public class PotentialFraudCase {
+public abstract class PotentialFraudCase {
 
     private String card;
+    private int type;
     private float totalAmount;
-    private Set<String> devices;
     private List<String> movements;
+    private Date createdAt;
 
     public PotentialFraudCase() {
         this.totalAmount = 0f;
-        this.devices = new HashSet<>();
         this.movements = new ArrayList<>();
+        this.createdAt = new Date();
     }
 
     public void addMovement(CardMovement cardMovement) {
         this.card = cardMovement.getCard();
         this.totalAmount += cardMovement.getAmount();
-        this.devices.add(cardMovement.getDevice());
         this.movements.add(cardMovement.getId());
     }
+
+    public abstract boolean isFraud();
 }
---
+++ fraud-checker/src/main/java/com/jaruiz/examples/kstreams/fraudchecker/model/PotentialPhysicalFraudCase.java
@@ -0,0 +1,40 @@
+package com.jaruiz.examples.kstreams.fraudchecker.model;
+
+import com.jaruiz.examples.kstreams.fraudchecker.config.FraudCheckerConfig;
+import com.jaruiz.examples.kstreams.model.CardMovement;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class PotentialPhysicalFraudCase extends PotentialFraudCase {
+
+    private Set<String> devices;
+
+    public PotentialPhysicalFraudCase() {
+        super();
+        this.devices = new HashSet<>();
+        setType(FraudCheckerConfig.MULTIPLE_PHYSICAL_MOVEMENTS_IN_SHORT_PERIOD);
+    }
+
+    public void addMovement(CardMovement cardMovement) {
+        super.addMovement(cardMovement);
+        devices.add(cardMovement.getDevice());
+    }
+
+    @Override
+    public boolean isFraud() {
+        // Different device within the session window
+        if (getDevices().size() > 1) {
+            return true;
+        }
+
+        // Same device but excessive amount of movements within the session window
+        if (getMovements().size() > 4) {
+            return true;
+        }
+
+        return false;
+    }
+}
